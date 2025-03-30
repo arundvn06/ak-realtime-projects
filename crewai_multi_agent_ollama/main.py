f7f56eab9883c7487b090e97d1287b69eb9ea36f
arundvn06 <arundvn06@gmail.com>
2025-03-30 20:07:07 +0530

    Multi agents using crewai framework running with local ollama - deepseak or llama

---
+++ crewai_multi_agent_ollama/main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from crewai_project_crew.crew import CrewaiProjectCrew
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'India and china ties',
+        'current_year': str(datetime.now().year)
+    }
+    
+    try:
+        result = CrewaiProjectCrew().crew().kickoff(inputs=inputs)
+        print(result)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        CrewaiProjectCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        CrewaiProjectCrew().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs",
+        "current_year": str(datetime.now().year)
+    }
+    try:
+        CrewaiProjectCrew().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
